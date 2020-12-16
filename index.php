ca1d2a17412c76c9d1d88dca75959e1fa6e7d546
ANAMULL <72936695+ANAMULL@users.noreply.github.com>
2020-12-16 10:19:26 -0800

    add my work

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shokher-School</title>
+<!--student tc-->
+<link rel="stylesheet" type="text/css" href="css/owl.main.css">
+<link rel="stylesheet" type="text/css" href="css/owl.theme.min.css">
+<link rel="stylesheet" type="text/css" href="css/testyslider.css">
+
+
+    <!--Bootstrap css-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!--Font Awesome  css-->
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!--Start navigation-->
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5 fixed-top">
+  <a class="navbar-brand" href="index.php"><img style="width:100px;height:50px;border-radius:50%" src="image/logo.png"></a>
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav custom-nav pl-5 ">
+     <li class="nav-item custom-nav-item"><a href="index.php" class="nav-link">Home</a></li> 
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Courses</a></li> 
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Payment</a></li> 
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">My Profile</a></li> 
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Logout</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Login</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Signup</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Feedback</a></li>
+     <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Contact</a></li>
+      
+    </ul>
+  </div>
+</nav>
+
+<!--Start navigation-->
+
+<!--Start background-->
+<div class="container-fluid remove-vid-marg">
+  <div class="vid-parent">
+    <video plysinline autoplay muted loop>
+      <source src="video/backgroun.mp4">
+</video>
+<div class="vid-overlay"></div>
+</div>
+<div class="vid-content">
+  <h1>Welcome to Shokher School School</div>
+
+</div>
+
+</div>
+<!--end background-->
+
+<!-- strat poupuler course-->
+<div class="container mt-5">
+  <h1 class="text-center">Populer Course</h1>
+<!-- strat poupuler course 1st card deck-->
+ <div class="card-deck mt-4">
+   <a href="#"class="btn" style="text-align:left;
+   padding:0px; margin:0px;"> 
+   <div class="card">
+     <img src="image/Artificial.jpg" class="card-img-top" alt="Guiter" />
+     <div class="card-body">
+       <h5 class="card-title"> Artificial intelligence</h5>
+       <p class="card-text">n publishing and graphic design, Lorem ipsum is a placeholder text 
+         commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content
+</p>
+</div>
+<div class="card-footer">
+  <p class="card-text d-inline">Price: <small><del>&
+    #8377 1000</del></small><span
+    class="font-weight-bolder">&#8377 700<span>
+</p>
+<a class="btn btn-primary text-white font-weight=bolder float-right"
+href="#">Enroll</a>
+</div>
+</div>
+</a>
+<a href="#"class="btn" style="text-align:left;
+   padding:0px; margin:0px;"> 
+   <div class="card">
+     <img src="image/reat js.png" class="card-img-top" alt="Guiter" />
+     <div class="card-body">
+       <h5 class="card-title"> React JS</h5>
+       <p class="card-text">n publishing and graphic design, Lorem ipsum is a placeholder text 
+         commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content
+</p>
+</div>
+<div class="card-footer">
+  <p class="card-text d-inline">Price: <small><del>&
+    #8377 2000</del></small><span
+    class="font-weight-bolder">&#8377 200<span>
+</p>
+<a class="btn btn-primary text-white font-weight=bolder float-right"
+href="#">Enroll</a>
+</div>
+</div>
+</a>
+<a href="#"class="btn" style="text-align:left;
+   padding:0px; margin:0px;"> 
+   <div class="card">
+     <img src="image/phpp.jpg" class="card-img-top" alt="Guiter" />
+     <div class="card-body">
+       <h5 class="card-title"> PHP learning Easy Way</h5>
+       <p class="card-text">n publishing and graphic design, Lorem ipsum is a placeholder text 
+         commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content
+</p>
+</div>
+<div class="card-footer">
+  <p class="card-text d-inline">Price: <small><del>&
+    #8377 2000</del></small><span
+    class="font-weight-bolder">&#8377 200<span>
+</p>
+<a class="btn btn-primary text-white font-weight=bolder float-right"
+href="#">Enroll</a>
+</div>
+</div>
+</a>
+</div>
+<!-- strat poupuler course 2st card deck-->
+<div class="card-deck mt-4">
+   <a href="#"class="btn" style="text-align:left;
+   padding:0px; margin:0px;"> 
+   <div class="card">
+     <img src="image/angular.png" class="card-img-top" alt="Guiter" />
+     <div class="card-body">
+       <h5 class="card-title"> Learn Python</h5>
+       <p class="card-text">n publishing and graphic design, Lorem ipsum is a placeholder text 
+         commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content
+</p>
+</div>
+<div class="card-footer">
+  <p class="card-text d-inline">Price: <small><del>&
+    #8377 2000</del></small><span
+    class="font-weight-bolder">&#8377 200<span>
+</p>
+<a class="btn btn-primary text-white font-weight=bolder float-right"
+href="#">Enroll</a>
+</div>
+</div>
+</a>
+
+<a href="#"class="btn" style="text-align:left;
+   padding:0px; margin:0px;"> 
+   <div class="card">
+     <img src="image/java.png" class="card-img-top" alt="Guiter" />
+     <div class="card-body">
+       <h5 class="card-title"> Learn Java</h5>
+       <p class="card-text">n publishing and graphic design, Lorem ipsum is a placeholder text 
+         commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content
+</p>
+</div>
+<div class="card-footer">
+  <p class="card-text d-inline">Price: <small><del>&
+    #8377 2000</del></small><span
+    class="font-weight-bolder">&#8377 200<span>
+</p>
+<a class="btn btn-primary text-white font-weight=bolder float-right"
+href="#">Enroll</a>
+</div>
+</div>
+</a>
+<a href="#"class="btn" style="text-align:left;
+   padding:0px; margin:0px;"> 
+   <div class="card">
+     <img src="image/english.jpg" class="card-img-top" alt="Guiter" />
+     <div class="card-body">
+       <h5 class="card-title"> Learn English</h5>
+       <p class="card-text">n publishing and graphic design, Lorem ipsum is a placeholder text 
+         commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content
+</p>
+</div>
+<div class="card-footer">
+  <p class="card-text d-inline">Price: <small><del>&
+    #8377 2000</del></small><span
+    class="font-weight-bolder">&#8377 200<span>
+</p>
+<a class="btn btn-primary text-white font-weight=bolder float-right"
+href="#">Enroll</a>
+</div>
+</div>
+</a>
+</div>
+
+<div class="text-center m-2">
+  <a class="btn btn-danger btn-sm" href="#">View All Courses</a>
+</div>
+</div>
+<!--end most populer course-->
+
+<!--contact us-->
+<div class="container mt-4" id="Contact">
+  <h2 class="text-center mb-4">Contact US</h2>
+  <div class="row">
+    <div class="col-md-8">
+      <form action="#" method="post">
+        <input type="text" class="form-control" name="name"
+        placeholder="Enter Name"><br>
+        <input type="text" class="form-control" name="subject"
+        placeholder="Subject"><br>
+        <input type="email" class="form-control" name="email"
+        placeholder="E-mail"><br>
+       <textarea class="form-control" name="message"
+       placeholder="Hoe can we helph you" style="height:150px;"></textarea><br>
+       <input class="btn btn-primary" type="submit"
+       value="Sand" name="submit"><br><br>
+</form>
+</div>
+
+<div class="col-md-4 stripe text-with text-center">
+  <h4>Shokher School</h4>
+  <p>Shokher School,
+    Near 220/D-Begum Rokeya Sarani<br>
+     (300 meter আইডিবি ভবন North Side of ঢাকা, 1207<br>
+     www.https://shokher_school.edu.bd/<br>
+</p>
+</div>
+</div>
+</div>
+
+
+
+<!--start student-->
+<div class="container-fluid mt-5" style="background-color:#4B7289
+" id="Feedback">
+<h1 class="text-center testyheading p-4"> Students Feedback</h1>
+<div class="row">
+  <div class="col-md-12">
+    <div id="testimonial-slider" class="owl-carousel">
+      <div class="testimonial">
+        <p class="description">
+        In publishing and graphic design, Lorem ipsum is a a
+         typeface without relying on meaningful conten</p>
+         <div class="pic">
+           <img src="image/linkdin.jpg">
+</div>
+<div class="testimonial-prof">
+  <h4>Anamul Haque<h4>
+    <small>Web Devolaper</small>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+<!--Jquary and Bootstrab javascript-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/poper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!--Font Awesome js-->
+    <script src="js/all.min.js"></script>
+
+    <!--student tc-->
+    <script type="text/javascript" src="js/owl.main.js"></script>
+    <script type="text/javascript" src="testyslider.js"></script>
+
+</body>
+</html>
